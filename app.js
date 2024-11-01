7112bc04b558f0d7f04dd3e0f54362116bb85a23
Erlend <erlendk.kodehode@gmail.com>
2024-11-01 11:25:42 +0100

    basic collision detection and obstacle generation

--- app.js
+++ app.js
@@ -1,17 +1,80 @@
 const box = document.querySelector("#box");
+const speedInput = document.querySelector("#speed");
+const container = document.querySelector("#container");
+const obstacles = document.querySelector("#obstacles");
+const obstaclesInput = document.querySelector("#obstaclesInput");
+const generateBtn = document.querySelector("#generateBtn");
+const ui = document.querySelector("#ui");
+const uiHeight = ui.getBoundingClientRect().height;
 let x = 0;
-let y = 0;
-const speed = 5;
+let y = uiHeight;
+let speed = parseInt(speedInput.value);
+let obstaclesnum = parseInt(obstaclesInput.value);
+let inElement = false;
+const yLimit = container.offsetHeight - box.offsetHeight;
+const xLimit = container.offsetWidth - box.offsetWidth;
+const boxHeight = box.getBoundingClientRect().height;
+const boxWidth = box.getBoundingClientRect().width;
+
 function updatePos() {
   y = parseInt(box.style.top.slice(0, box.style.top.indexOf("p")));
   x = parseInt(box.style.left.slice(0, box.style.left.indexOf("p")));
 }
 
-updatePos();
+function collisionDetect() {
+  for (const obstacle of obstacles.children) {
+    const obstX = obstacle.getBoundingClientRect().x;
+    const obstY = obstacle.getBoundingClientRect().y;
+    const obstWidth = obstacle.getBoundingClientRect().width;
+    const obstHeight = obstacle.getBoundingClientRect().height;
+    if (
+      x + boxWidth / 2 >= obstX - obstWidth / 2 &&
+      x - boxWidth / 2 <= obstX + obstWidth / 2 &&
+      y + boxHeight / 2 >= obstY - obstHeight / 2 &&
+      y - boxHeight / 2 <= obstY + obstHeight / 2
+    ) {
+      console.log("hit");
+    }
+  }
+}
+
+box.style.top = `${y}px`;
+box.style.left = `${x}px`;
+
+for (const element of ui.children) {
+  element.addEventListener("mouseenter", () => (inElement = true));
+  element.addEventListener("mouseleave", () => (inElement = false));
+}
+
+obstaclesInput.addEventListener(
+  "input",
+  () => (obstaclesnum = parseInt(obstaclesInput.value))
+);
+
+speedInput.addEventListener(
+  "input",
+  () => (speed = parseInt(speedInput.value))
+);
+
+generateBtn.addEventListener("click", () => {
+  const obstacleContainer = obstacles.children;
+  for (let i = obstacleContainer.length - 1; i >= 0; i--) {
+    obstacleContainer[i].remove();
+  }
+
+  for (let i = 0; i < obstaclesnum; i++) {
+    const obstacle = document.createElement("p");
+    obstacle.className = "obstacle";
+    obstacles.append(obstacle);
+    obstacle.style.top = `${Math.floor(Math.random() * yLimit) + uiHeight}px`;
+    obstacle.style.left = `${Math.floor(Math.random() * xLimit)}px`;
+  }
+});
 
 document.body.addEventListener("click", (e) => {
-  box.style.top = `${e.y - box.offsetWidth / 2}px`;
-  box.style.left = `${e.x - box.offsetHeight / 2}px`;
+  if (inElement) return;
+  box.style.top = `${e.y - boxHeight / 2}px`;
+  box.style.left = `${e.x - boxHeight / 2}px`;
   updatePos();
 });
 
@@ -19,24 +82,26 @@ window.addEventListener("keydown", (e) => {
   switch (e.key) {
     case "ArrowUp":
     case "w":
-      y -= speed;
+      y = y - speed > uiHeight ? y - speed : uiHeight;
       break;
 
     case "ArrowDown":
     case "s":
-      y += speed;
+      y = y + speed < yLimit ? y + speed : yLimit;
       break;
 
     case "ArrowLeft":
     case "a":
-      x -= speed;
+      x = x - speed > 0 ? x - speed : 0;
       break;
 
     case "ArrowRight":
     case "d":
-      x += speed;
+      x = x + speed < xLimit ? x + speed : xLimit;
       break;
   }
+  collisionDetect();
+
   box.style.top = `${y}px`;
   box.style.left = `${x}px`;
 });
