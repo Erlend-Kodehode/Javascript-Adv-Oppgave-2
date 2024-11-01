bb1d3804c750da1e0c56b7755ee74f63a678b842
Erlend <erlendk.kodehode@gmail.com>
2024-11-01 14:47:42 +0100

    comments and bug fixes

--- app.js
+++ app.js
@@ -11,89 +11,105 @@ let y = uiHeight;
 let prevX = 0;
 let prevY = 0;
 let speed = parseInt(speedInput.value);
-let obstaclesnum = parseInt(obstaclesInput.value);
+let obstaclesNum = parseInt(obstaclesInput.value);
 let inElement = false;
-const yLimit = container.offsetHeight - box.offsetHeight;
-const xLimit = container.offsetWidth - box.offsetWidth;
+//gets the height and width of the box
 const boxHeight = box.getBoundingClientRect().height;
 const boxWidth = box.getBoundingClientRect().width;
-
-function updatePos() {
-  y = parseInt(box.style.top.slice(0, box.style.top.indexOf("p")));
-  x = parseInt(box.style.left.slice(0, box.style.left.indexOf("p")));
-}
+// sets the limits for the x and y variables to be within the page accounting for the boxes width and height
+const yLimit = container.getBoundingClientRect().height - boxHeight;
+const xLimit = container.getBoundingClientRect().width - boxWidth;
 
 function collisionDetect() {
+  // goes through all the obstacles on the page
   for (const obstacle of obstacles.children) {
     const obstX = obstacle.getBoundingClientRect().x;
     const obstY = obstacle.getBoundingClientRect().y;
     const obstWidth = obstacle.getBoundingClientRect().width;
     const obstHeight = obstacle.getBoundingClientRect().height;
+    //checks if the box is inside of an obstacle
     if (
-      x + boxWidth / 2 > obstX - obstWidth / 2 &&
-      x - boxWidth / 2 < obstX + obstWidth / 2 &&
-      y + boxHeight / 2 > obstY - obstHeight / 2 &&
-      y - boxHeight / 2 < obstY + obstHeight / 2
+      x + boxWidth > obstX &&
+      x < obstX + obstWidth &&
+      y + boxHeight > obstY &&
+      y < obstY + obstHeight
     ) {
+      //sets the x or the y to the correct edge of the box depending on the direction the box was heading
       if (y < prevY) {
         y = obstY + obstHeight;
       } else if (y > prevY) {
-        y = obstY - obstHeight;
+        y = obstY - boxHeight;
       } else if (x < prevX) {
         x = obstX + obstWidth;
       } else if (x > prevX) {
-        x = obstX - obstWidth;
+        x = obstX - boxWidth;
       }
     }
   }
 }
-
+// makes it so the box starts in the correct position
 box.style.top = `${y}px`;
 box.style.left = `${x}px`;
 
+// disables click to position if the mouse is in any of the ui elements
 for (const element of ui.children) {
   element.addEventListener("mouseenter", () => (inElement = true));
   element.addEventListener("mouseleave", () => (inElement = false));
 }
 
-obstaclesInput.addEventListener(
-  "input",
-  () => (obstaclesnum = parseInt(obstaclesInput.value))
-);
-
+// updates the speed variable whenever the input changes
 speedInput.addEventListener(
   "input",
   () => (speed = parseInt(speedInput.value))
 );
 
 generateBtn.addEventListener("click", () => {
+  //removes the previous obstacles
   const obstacleContainer = obstacles.children;
   for (let i = obstacleContainer.length - 1; i >= 0; i--) {
     obstacleContainer[i].remove();
   }
 
-  for (let i = 0; i < obstaclesnum; i++) {
-    const obstacle = document.createElement("p");
+  //creates a number of obstacles determined by the input value
+  obstaclesNum = parseInt(obstaclesInput.value);
+  for (let i = 0; i < obstaclesNum; i++) {
+    const obstacle = document.createElement("div");
+    //gives it a class for styling
     obstacle.className = "obstacle";
-    obstacles.append(obstacle);
+    //gives it a random position
     obstacle.style.top = `${Math.floor(Math.random() * yLimit) + uiHeight}px`;
     obstacle.style.left = `${Math.floor(Math.random() * xLimit)}px`;
+
+    obstacles.append(obstacle);
   }
 });
 
+//click to position
 document.body.addEventListener("click", (e) => {
+  //immediately returns if the mouse is in an ui element
   if (inElement) return;
-  box.style.top = `${e.y - boxHeight / 2}px`;
-  box.style.left = `${e.x - boxHeight / 2}px`;
-  updatePos();
+
+  //offsets the coordinates to center the box
+  const posY = e.y - boxHeight / 2;
+  const posX = e.x - boxWidth / 2;
+  //sets the position of the box to where the mouse clicked
+  box.style.top = `${posY}px`;
+  box.style.left = `${posX}px`;
+  //updates the x and y variables
+  x = posX;
+  y = posY;
 });
 
 window.addEventListener("keydown", (e) => {
+  //stores the x and y variables before they change
   prevY = y;
   prevX = x;
+
+  //moves the box in a direction depending on which key was pressed
   switch (e.key) {
     case "ArrowUp":
     case "w":
+      //checks if the next position is inside or above the top of the page and if it is, it sets the position to be against the edge instead
       y = y - speed > uiHeight ? y - speed : uiHeight;
       break;
 
@@ -112,8 +128,11 @@ window.addEventListener("keydown", (e) => {
       x = x + speed < xLimit ? x + speed : xLimit;
       break;
   }
+
+  //runs the collision detection
   collisionDetect();
 
+  //updates the position
   box.style.top = `${y}px`;
   box.style.left = `${x}px`;
 });
